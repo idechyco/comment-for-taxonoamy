a9ec184f3b0451454b2bdfb01eead6326c89772f
alirezajahan13 <107421412+alirezajahan13@users.noreply.github.com>
2024-05-29 16:25:38 +0330

    Create comments-for-taxonomy.php

---
+++ comments-for-taxonomy.php
@@ -0,0 +1,36 @@
+<?php
+/*
+Plugin Name:  Comments For Taxonomy
+Plugin URI:   https://idechy.ir/
+Description:  کامنت
+Version:      1.0
+Author:       idechy
+Author URI:   https://idechy.ir/
+License:      GPL2
+License URI:  https://www.gnu.org/licenses/gpl-2.0.html
+Text Domain:  wpb-tutorial
+Domain Path:  /languages
+*/
+
+
+if ( ! defined( 'ABSPATH' ) ) {
+    exit();
+}
+
+
+function bbloomer_shop_product_short_description() {
+	the_excerpt();
+}
+
+function my_excerpt_length($length){
+	return 20;
+}
+add_filter('excerpt_length', 'my_excerpt_length');
+
+function new_excerpt_more( $more ) {
+	return ' ... ';
+}
+add_filter('excerpt_more', 'new_excerpt_more');
+
+
+?>
